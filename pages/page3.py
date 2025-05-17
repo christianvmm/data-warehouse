e76b510d98fb72d1aa15b3ba2e9ba222ba14cb3e
Christian Velez <christian.gerardo_@outlook.es>
2025-05-16 18:11:49 -0600

    zzz

--- pages/page3.py
+++ pages/page3.py
@@ -2,6 +2,6 @@ from dash import html
 
 def render_page3():
     return html.Div([
-        html.H3("Welcome to Page 3"),
+        html.H3("Welcome to Page 2"),
         html.Div(id="data-check")
     ])
